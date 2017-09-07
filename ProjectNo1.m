c27a849dd0c4d6bba29a3c1bcda9680ecfd6f1cd
eiriknne <31724961+eiriknne@users.noreply.github.com>
2017-09-07 21:04:57 +0200

    Gjorde ferdig plottet og prøver meg frem med publish-funksjonen i matlab. Latex i matlab

--- ProjectNo1.m
+++ ProjectNo1.m
@@ -1,28 +1,46 @@
+%% *Project nr. 1.*
+% _Groupe 3. Tobias and Eirik^2_
+%
+%% *Part I (i):Substitutional-self diffusion (“tracer diffusion”) - deriving analytical the equation*
+%
+% $$C(x,t)=\frac{C_1+C_2}{2}-\frac{C_1+C_2}{2}\cdot erf \big( \frac{x}{2\sqrt{Dt}} \big)$$ 
+%
+
+%% *Part I (ii):Substitutional-self diffusion (“tracer diffusion”) - modeling coupled bar*
+%
+
 clear all
 close all
 
-% EirikNN: Starter å lage scriptet.
-
-D = ??  % ref: D. Porter, K Easterling, M. Sherif. Phase Transformations in Metal and Alloys. Third edition. tabel 2.2
+D = 10^-5; % ref: D. Porter, K Easterling, M. Sherif. Phase Transformations in Metal and Alloys. Third edition. tabel 2.2
 C1 = 1;
 C2 = 0;
 
 % Analytical solution of ficks law:
 funCxt = @(x,t) (C1 + C2)/2 - ((C1 - C2)/2) * erf(x/(D*t));
 
-t = [0];    % [s]
-dt = 100;   % [s]
+t = 0;    % [s]
+dt = 1000;  % [s]
 C = [];     % [quantety m^3] composition
-x = linspace(-1,1,20); % [mm] position
+x = linspace(-1,1,50); % [mm] position
 
 i = 0;
 
-while t > 30*60*60 % [s] run for 30 hours
-i = i +1;
-    
+while t < 30*60*60 % [s] run for 30 hours
+i = i +1;  
 	for j = 1:length(x)
-        C(i,j) = funCxt(x(j),t(i)) 
+        C(i,j) = funCxt(x(j),t(i)); 
     end 
 t(i+1) = t(i) + dt;
-
 end
+
+% Plotting consentration profile at 5 different times:
+j = 1;
+for i = floor(linspace(1,(length(t)-1),5))
+    legends{j} = [num2str(round(t(i)/60/60,3)),' hours'];
+    j = j+1; % counter for legend
+    plot(x,C(i,:))
+    hold on
+end
+
+legend(legends)
