78d2a9c987cf659b251acec2ae2d23a11e5b9052
eiriknne <31724961+eiriknne@users.noreply.github.com>
2017-09-07 11:30:10 +0200

    Committing first time
    Commitng to master

---
+++ Test.m
@@ -0,0 +1,23 @@
+
+%%
+% *Project No.1*
+% 
+% _Written by Eirik, Eirik and Tobias_
+%
+% Innledning
+%
+% Utledning:
+%
+% $C(x,t)=\frac{C_1+C_2}{2}-\frac{C_1+C_2}{2}\cdot erf \big( \frac{x}{2\sqrt{Dt}} \big)$ 
+% 
+
+%%
+
+disp('Helo word')
+disp('test branch')
+
+
+
+JHVH
+IHBJKHB
+IHVJKH
